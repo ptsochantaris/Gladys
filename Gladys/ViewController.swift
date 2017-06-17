dc71992a9b4c02bdeb9a44b67faaa272b5885a67
Paul Tsochantaris <ptsochantaris@icloud.com>
2017-06-17 15:15:16 +0100

    Async loading v0.1, added images and title v.01

--- Gladys/ViewController.swift
+++ Gladys/ViewController.swift
@@ -9,14 +9,22 @@
 import UIKit
 import MapKit
 
+protocol LoadCompletionDelegate: class {
+	func loadCompleted(success: Bool)
+}
+
 final class ArchivedDropItemType {
 
 	let typeIdentifier: String
+	var classType: ClassType?
 
 	private var bytes: Data?
-	private var classType: ClassType?
 	private let folderUrl: URL
 	private let uuid = UUID()
+
+	private weak var delegate: LoadCompletionDelegate?
+	private var loadCount = 0
+	private var allLoadedWell = true
 
 	func setBytes(object: Any, classType: ClassType) {
 		let d = NSMutableData()
@@ -31,8 +39,9 @@ final class ArchivedDropItemType {
 		case NSString, NSAttributedString, UIColor, UIImage, NSData, MKMapItem, NSURL
 	}
 
-	init(provider: NSItemProvider, typeIdentifier: String, parentUrl: URL) {
+	init(provider: NSItemProvider, typeIdentifier: String, parentUrl: URL, delegate: LoadCompletionDelegate) {
 		self.typeIdentifier = typeIdentifier
+		self.delegate = delegate
 		self.folderUrl = parentUrl.appendingPathComponent(uuid.uuidString)
 
 		provider.loadItem(forTypeIdentifier: typeIdentifier, options: nil) { item, error in
@@ -42,29 +51,35 @@ final class ArchivedDropItemType {
 			}
 
 			if let item = item as? NSString {
-				NSLog("      received string object: \(item)")
+				NSLog("      received string: \(item)")
 				self.setBytes(object: item, classType: .NSString)
+				self.signalDone()
 
 			} else if let item = item as? NSAttributedString {
-				NSLog("      received attributed string object: \(item)")
+				NSLog("      received attributed string: \(item)")
 				self.setBytes(object: item, classType: .NSAttributedString)
+				self.signalDone()
 
 			} else if let item = item as? UIColor {
-				NSLog("      received color object: \(item)")
+				NSLog("      received color: \(item)")
 				self.setBytes(object: item, classType: .UIColor)
+				self.signalDone()
 
 			} else if let item = item as? UIImage {
-				NSLog("      received image object: \(item)")
+				NSLog("      received image: \(item)")
 				self.setBytes(object: item, classType: .UIImage)
+				self.signalDone()
 
 			} else if let item = item as? Data {
 				NSLog("      received data: \(item)")
 				self.classType = .NSData
 				self.bytes = item
+				self.signalDone()
 
 			} else if let item = item as? MKMapItem {
 				NSLog("      received map item: \(item)")
 				self.setBytes(object: item, classType: .MKMapItem)
+				self.signalDone()
 
 			} else if let item = item as? URL {
 				if item.scheme == "file" {
@@ -74,21 +89,38 @@ final class ArchivedDropItemType {
 							NSLog("      received local url: \(url)")
 							let localUrl = self.copyLocal(url)
 							self.setBytes(object: localUrl, classType: .NSURL)
+							self.signalDone()
+
 						} else if let error = error {
 							NSLog("Error fetching local url file representation: \(error.localizedDescription)")
+							self.allLoadedWell = false
+							self.signalDone()
 						}
 					}
 				} else {
 					NSLog("      received remote url: \(item)")
 					self.setBytes(object: item, classType: .NSURL)
+					self.signalDone()
 				}
+
 			} else if let error = error {
 				NSLog("      error receiving item: \(error.localizedDescription)")
+				self.allLoadedWell = false
+				self.signalDone()
+
 
 			} else {
 				NSLog("      unknown class")
+				self.allLoadedWell = false
+				self.signalDone()
 				// TODO: generate analyitics report to record what type was received and what UTI
 			}
+		}
+	}
+
+	private func signalDone() {
+		DispatchQueue.main.async {
+			self.delegate?.loadCompleted(success: self.allLoadedWell)
 		}
 	}
 
@@ -125,13 +157,95 @@ final class ArchivedDropItemType {
 			completion(nil, nil)
 		}
 	}
+
+	var displayIcon: UIImage? {
+		if let data = self.bytes {
+			if classType == .UIImage {
+				let u = NSKeyedUnarchiver(forReadingWith: data)
+				return u.decodeObject(of: [UIImage.self], forKey: ClassType.UIImage.rawValue) as? UIImage
+			} else if typeIdentifier == "public.png" || typeIdentifier == "public.jpeg" {
+				if classType == .NSURL {
+					let u = NSKeyedUnarchiver(forReadingWith: data)
+					if let url = u.decodeObject(of: [NSURL.self], forKey: ClassType.NSURL.rawValue) as? NSURL, let path = url.path {
+						return UIImage(contentsOfFile: path)
+					} else {
+						return nil
+					}
+				} else if classType == .NSData {
+					return UIImage(data: data)
+				}
+			}
+		}
+		return nil
+	}
+
+	var displayTitle: String? {
+		if let data = self.bytes {
+			if classType == .NSString {
+				let u = NSKeyedUnarchiver(forReadingWith: data)
+				if let res = u.decodeObject(of: [NSString.self], forKey: ClassType.NSString.rawValue) as? String {
+					return res
+				}
+			} else if classType == .NSAttributedString {
+				let u = NSKeyedUnarchiver(forReadingWith: data)
+				let a = u.decodeObject(of: [NSAttributedString.self], forKey: ClassType.NSAttributedString.rawValue) as? NSAttributedString
+				if let res = a?.string {
+					return res
+				}
+			} else if classType == .NSURL {
+				let u = NSKeyedUnarchiver(forReadingWith: data)
+				let a = u.decodeObject(of: [NSURL.self], forKey: ClassType.NSURL.rawValue) as? NSURL
+				if let res = a?.absoluteString {
+					return res
+				}
+			} else if typeIdentifier == "public.utf8-plain-text" {
+				return String(data: data, encoding: .utf8)
+			} else if typeIdentifier == "public.utf16-plain-text" {
+				return String(data: data, encoding: .utf16)
+			}
+		}
+		return nil
+
+	}
 }
 
-final class ArchivedDropItem {
+final class ArchivedDropItem: LoadCompletionDelegate {
 
 	let uuid = UUID()
 	let suggestedName: String?
 	var typeItems: [ArchivedDropItemType]!
+
+	weak var delegate: LoadCompletionDelegate?
+	private var loadCount = 0
+	private var allLoadedWell = true
+	func loadCompleted(success: Bool) {
+		if !success { allLoadedWell = false }
+		loadCount = loadCount - 1
+		if loadCount == 0 {
+			delegate?.loadCompleted(success: allLoadedWell)
+		}
+	}
+
+	var displayIcon: UIImage? {
+		for i in typeItems {
+			if let img = i.displayIcon {
+				return img
+			}
+		}
+		return nil
+	}
+
+	var displayTitle: String? {
+		if let suggestedName = suggestedName {
+			return suggestedName
+		}
+		for i in typeItems {
+			if let title = i.displayTitle {
+				return title
+			}
+		}
+		return nil
+	}
 
 	var myURL: URL {
 		let f = FileManager.default
@@ -139,9 +253,11 @@ final class ArchivedDropItem {
 		return docs.appendingPathComponent(uuid.uuidString)
 	}
 
-	init(provider: NSItemProvider) {
+	init(provider: NSItemProvider, delegate: LoadCompletionDelegate) {
+		self.delegate = delegate
 		suggestedName = provider.suggestedName
-		typeItems = provider.registeredTypeIdentifiers.map { ArchivedDropItemType(provider: provider, typeIdentifier: $0, parentUrl: myURL) }
+		loadCount = provider.registeredTypeIdentifiers.count
+		typeItems = provider.registeredTypeIdentifiers.map { ArchivedDropItemType(provider: provider, typeIdentifier: $0, parentUrl: myURL, delegate: self) }
 	}
 
 	var itemProvider: NSItemProvider {
@@ -154,19 +270,46 @@ final class ArchivedDropItem {
 	}
 }
 
-final class ArchivedDrop {
+final class ArchivedDrop: LoadCompletionDelegate {
 
 	private let uuid = UUID()
 	private let createdAt = Date()
-	private let items: [ArchivedDropItem]
+	private var items: [ArchivedDropItem]!
 
 	var displayIcon: UIImage {
+		for i in items {
+			if let img = i.displayIcon {
+				return img
+			}
+		}
 		return #imageLiteral(resourceName: "iconStickyNote") // TODO: use image element from items, if available, or possibly draw from text elements
 	}
 
-	static var counter = 0
+	var displayTitle: String {
+		if isLoading {
+			return "..."
+		}
 
-	var displayLabel: String
+		for i in items {
+			if let title = i.displayTitle {
+				return title
+			}
+		}
+		return "\(createdAt.timeIntervalSinceReferenceDate)" // TODO
+	}
+
+	weak var delegate: LoadCompletionDelegate?
+	private var loadCount = 0
+	private var allLoadedWell = true
+	var isLoading = true
+	func loadCompleted(success: Bool) {
+		if !success { allLoadedWell = false }
+		loadCount = loadCount - 1
+		if loadCount == 0 {
+			isLoading = false
+			delegate?.loadCompleted(success: allLoadedWell)
+		}
+	}
 
 	var dragItems: [UIDragItem] {
 		return items.map {
@@ -181,25 +324,40 @@ final class ArchivedDrop {
 		let progressType = session.progressIndicatorStyle
 		NSLog("Should display progress: \(progressType)")
 
-		ArchivedDrop.counter = ArchivedDrop.counter + 1
-		displayLabel = "Some data \(ArchivedDrop.counter)" // TODO: enahnce with sugegsted names of elements, date, or quote text elements
-
-		self.items = session.items.map {
-			return ($0.localObject as? ArchivedDropItem)
-				?? ArchivedDropItem(provider: $0.itemProvider)
+		loadCount = session.items.count
+		items = session.items.map {
+			if let item = ($0.localObject as? ArchivedDropItem) {
+				item.delegate = self
+				return item
+			} else {
+				return ArchivedDropItem(provider: $0.itemProvider, delegate: self)
+			}
 		}
 	}
 }
 
-final class ArchivedItemCell: UICollectionViewCell {
+final class ArchivedItemCell: UICollectionViewCell, LoadCompletionDelegate {
 	@IBOutlet weak var image: UIImageView!
 	@IBOutlet weak var label: UILabel!
 
 	var archivedDrop: ArchivedDrop? {
 		didSet {
-			image.image = archivedDrop?.displayIcon
-			label.text = archivedDrop?.displayLabel
+			oldValue?.delegate = nil
+			if archivedDrop?.isLoading ?? true {
+				image.isHidden = true
+			} else {
+				image.image = archivedDrop?.displayIcon
+			}
+			archivedDrop?.delegate = self
+			label.text = archivedDrop?.displayTitle
 		}
+	}
+
+	func loadCompleted(success: Bool) {
+		image.image = archivedDrop?.displayIcon
+		image.isHidden = false
+		label.text = archivedDrop?.displayTitle
+		NSLog("load complete for drop group")
 	}
 }
 
@@ -235,8 +393,8 @@ final class ViewController: UIViewController, UICollectionViewDelegate, UICollec
 
 			NSLog("local drop")
 
-			let destinationIndexPath = coordinator.destinationIndexPath ?? IndexPath(item: archivedDrops.count-1, section: 0)
-			if let firstDragItem = coordinator.items.first,
+			if let destinationIndexPath = coordinator.destinationIndexPath,
+				let firstDragItem = coordinator.items.first,
 				let existingItem = firstDragItem.dragItem.localObject as? ArchivedDrop,
 				let previousIndex = firstDragItem.sourceIndexPath {
 
@@ -285,6 +443,7 @@ final class ViewController: UIViewController, UICollectionViewDelegate, UICollec
 	}
 
 	func collectionView(_ collectionView: UICollectionView, dropSessionDidEnd session: UIDropSession) {
+		// TODO: possibe bug
 		if collectionView.numberOfItems(inSection: 0) != archivedDrops.count {
 			collectionView.endInteractiveMovement()
 		}
@@ -296,7 +455,7 @@ final class ViewController: UIViewController, UICollectionViewDelegate, UICollec
 		archivedItemCollectionView.delegate = self
 		archivedItemCollectionView.dropDelegate = self
 		archivedItemCollectionView.dragDelegate = self
-		archivedItemCollectionView.reorderingCadence = .fast
+		archivedItemCollectionView.reorderingCadence = .immediate
 	}
 }
 
