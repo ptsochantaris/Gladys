71754ac570bd52977600e7cdb3c1b2881d3c8c6f
Paul Tsochantaris <ptsochantaris@icloud.com>
2017-06-22 23:13:59 +0100

    Title improvement, better build options

--- Gladys/Model/Types.swift
+++ Gladys/Model/Types.swift
@@ -7,6 +7,14 @@ struct ArchivedDropDisplayInfo {
 	let title: String?
 	let accessoryText: String?
 	let titleAlignment: NSTextAlignment
+
+	init(image: UIImage?, imageContentMode: ArchivedDropItemDisplayType, title: String?, accessoryText: String?, titleAlignment: NSTextAlignment) {
+		self.image = image
+		self.imageContentMode = imageContentMode
+		self.title = title
+		self.accessoryText = (accessoryText != title) ? accessoryText : nil
+		self.titleAlignment = titleAlignment
+	}
 }
 
 enum ArchivedDropItemDisplayType: Int {
