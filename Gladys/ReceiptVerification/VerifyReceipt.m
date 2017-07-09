25de94bd90bff7b11ac351306d8f6bd53a37a577
Paul Tsochantaris <ptsochantaris@icloud.com>
2017-07-09 19:20:43 +0100

    Checking valid transaction in receipt

--- Gladys/ReceiptVerification/VerifyReceipt.m
+++ Gladys/ReceiptVerification/VerifyReceipt.m
@@ -5,6 +5,115 @@
 #import <openssl/x509.h>
 #import <openssl/evp.h>
 
+NSDateFormatter *makeFormatter() {
+	// Date formatter to handle RFC 3339 dates in GMT time zone
+	NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+	[formatter setLocale:[[NSLocale alloc] initWithLocaleIdentifier:@"en_US_POSIX"]];
+	[formatter setDateFormat:@"yyyy'-'MM'-'dd'T'HH':'mm':'ss'Z'"];
+	[formatter setTimeZone:[NSTimeZone timeZoneForSecondsFromGMT:0]];
+	return formatter;
+}
+
+BOOL checkPayload(const unsigned char *ptr, long len) {
+	const unsigned char *end = ptr + len;
+	const unsigned char *str_ptr;
+
+	int type = 0, str_type = 0;
+	int xclass = 0, str_xclass = 0;
+	long length = 0, str_length = 0;
+
+	NSString *productIdentifier;
+	//NSString *transactionIdentifier;
+	//NSDate *purchaseDate;
+
+	//NSDateFormatter *formatter = makeFormatter();
+
+	// Decode payload (a SET is expected)
+	ASN1_get_object(&ptr, &length, &type, &xclass, end - ptr);
+	if (type != V_ASN1_SET) {
+		return NO;
+	}
+
+	while (ptr < end) {
+		ASN1_INTEGER *integer;
+
+		// Parse the attribute sequence (a SEQUENCE is expected)
+		ASN1_get_object(&ptr, &length, &type, &xclass, end - ptr);
+		if (type != V_ASN1_SEQUENCE) {
+			return NO;
+		}
+
+		const unsigned char *seq_end = ptr + length;
+		long attr_type = 0;
+		long attr_version = 0;
+
+		// Parse the attribute type (an INTEGER is expected)
+		ASN1_get_object(&ptr, &length, &type, &xclass, end - ptr);
+		if (type != V_ASN1_INTEGER) {
+			return NO;
+		}
+		integer = c2i_ASN1_INTEGER(NULL, &ptr, length);
+		attr_type = ASN1_INTEGER_get(integer);
+		ASN1_INTEGER_free(integer);
+
+		// Parse the attribute version (an INTEGER is expected)
+		ASN1_get_object(&ptr, &length, &type, &xclass, end - ptr);
+		if (type != V_ASN1_INTEGER) {
+			return NO;
+		}
+		integer = c2i_ASN1_INTEGER(NULL, &ptr, length);
+		attr_version = ASN1_INTEGER_get(integer);
+		ASN1_INTEGER_free(integer);
+
+		// Check the attribute value (an OCTET STRING is expected)
+		ASN1_get_object(&ptr, &length, &type, &xclass, end - ptr);
+		if (type != V_ASN1_OCTET_STRING) {
+			return NO;
+		}
+
+		switch (attr_type) {
+
+			case 1702:
+				// Product identifier
+				str_ptr = ptr;
+				ASN1_get_object(&str_ptr, &str_length, &str_type, &str_xclass, seq_end - str_ptr);
+				if (str_type == V_ASN1_UTF8STRING) {
+					// We store the decoded string for later
+					productIdentifier = [[NSString alloc] initWithBytes:str_ptr length:str_length encoding:NSUTF8StringEncoding];
+				}
+				break;
+
+				/*
+			case 1703:
+				// Transaction identifier
+				str_ptr = ptr;
+				ASN1_get_object(&str_ptr, &str_length, &str_type, &str_xclass, seq_end - str_ptr);
+				if (str_type == V_ASN1_UTF8STRING) {
+					// We store the decoded string for later
+					transactionIdentifier = [[NSString alloc] initWithBytes:str_ptr length:str_length encoding:NSUTF8StringEncoding];
+				}
+				break;
+
+			case 1704:
+				// Purchase date
+				str_ptr = ptr;
+				ASN1_get_object(&str_ptr, &str_length, &str_type, &str_xclass, seq_end - str_ptr);
+				if (str_type == V_ASN1_IA5STRING) {
+					// The date is stored as a string that needs to be parsed
+					NSString *dateString = [[NSString alloc] initWithBytes:str_ptr length:str_length encoding:NSASCIIStringEncoding];
+					purchaseDate = [formatter dateFromString:dateString];
+				}
+				break;
+				 */
+		}
+
+		// Move past the value
+		ptr += length;
+	}
+
+	return [productIdentifier isEqualToString:@"INFINITE"];
+}
+
 BOOL verifyIapReceipt() {
 
 	NSURL *dataUrl = [[NSBundle mainBundle] appStoreReceiptURL];
@@ -72,12 +181,9 @@ BOOL verifyIapReceipt() {
 	NSData *hashData = nil;
 	NSData *opaqueData = nil;
 	NSDate *expirationDate = nil;
+	BOOL haveValidTransaction = NO;
 
-	// Date formatter to handle RFC 3339 dates in GMT time zone
-	NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
-	[formatter setLocale:[[NSLocale alloc] initWithLocaleIdentifier:@"en_US_POSIX"]];
-	[formatter setDateFormat:@"yyyy'-'MM'-'dd'T'HH':'mm':'ss'Z'"];
-	[formatter setTimeZone:[NSTimeZone timeZoneForSecondsFromGMT:0]];
+	NSDateFormatter *formatter = makeFormatter();
 
 	// Decode payload (a SET is expected)
 	ASN1_get_object(&ptr, &length, &type, &xclass, end - ptr);
@@ -123,7 +229,7 @@ BOOL verifyIapReceipt() {
 		}
 
 		switch (attr_type) {
-				case 2:
+			case 2:
 				// Bundle identifier
 				str_ptr = ptr;
 				ASN1_get_object(&str_ptr, &str_length, &str_type, &str_xclass, seq_end - str_ptr);
@@ -135,7 +241,7 @@ BOOL verifyIapReceipt() {
 				}
 				break;
 
-				case 3:
+			case 3:
 				// Bundle version
 				str_ptr = ptr;
 				ASN1_get_object(&str_ptr, &str_length, &str_type, &str_xclass, seq_end - str_ptr);
@@ -145,17 +251,24 @@ BOOL verifyIapReceipt() {
 				}
 				break;
 
-				case 4:
+			case 4:
 				// Opaque value
 				opaqueData = [[NSData alloc] initWithBytes:(const void *)ptr length:length];
 				break;
 
-				case 5:
+			case 5:
 				// Computed GUID (SHA-1 Hash)
 				hashData = [[NSData alloc] initWithBytes:(const void *)ptr length:length];
 				break;
 
-				case 21:
+			case 17:
+				// Transaction
+				if (!haveValidTransaction) {
+					haveValidTransaction = checkPayload(ptr, length);
+				}
+				break;
+
+			case 21:
 				// Expiration date
 				str_ptr = ptr;
 				ASN1_get_object(&str_ptr, &str_length, &str_type, &str_xclass, seq_end - str_ptr);
@@ -178,19 +291,20 @@ BOOL verifyIapReceipt() {
 	if (bundleIdString == nil ||
 		bundleVersionString == nil ||
 		opaqueData == nil ||
-		hashData == nil) {
+		hashData == nil ||
+		!haveValidTransaction) {
 
 		return NO;
 	}
 
 	// Check the bundle identifier
-	if (![bundleIdString isEqualToString:[NSBundle mainBundle].bundleIdentifier]) {
+	if (![bundleIdString isEqualToString:@"build.bru.Gladys"]) {
 		return NO;
 	}
 
 	// Check the bundle version
 	//if (![bundleVersionString isEqualToString:[[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleVersion"]]) {
-		//return NO;
+	//return NO;
 	//}
 
 	UIDevice *device = [UIDevice currentDevice];
