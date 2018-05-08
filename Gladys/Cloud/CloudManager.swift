614a8c56400ebffa36462eb8135fea4e63bef708
Paul Tsochantaris <ptsochantaris@icloud.com>
2018-05-08 01:06:25 +0100

    Cloud types cleanup

--- Gladys/Cloud/CloudManager.swift
+++ Gladys/Cloud/CloudManager.swift
@@ -11,6 +11,40 @@ import UIKit
 #endif
 import CloudKit
 
+let diskSizeFormatter = ByteCountFormatter()
+
+extension Array {
+	func bunch(maxSize: Int) -> [[Element]] {
+		var pos = 0
+		var res = [[Element]]()
+		while pos < count {
+			let end = Swift.min(count, pos + maxSize)
+			let a = self[pos ..< end]
+			res.append(Array(a))
+			pos += maxSize
+		}
+		return res
+	}
+}
+
+extension Array where Element == [CKRecord] {
+	func flatBunch(minSize: Int) -> [[CKRecord]] {
+		var result = [[CKRecord]]()
+		var newChild = [CKRecord]()
+		for childArray in self {
+			newChild.append(contentsOf: childArray)
+			if newChild.count >= minSize {
+				result.append(newChild)
+				newChild.removeAll(keepingCapacity: true)
+			}
+		}
+		if newChild.count > 0 {
+			result.append(newChild)
+		}
+		return result
+	}
+}
+
 final class CloudManager {
 
 	static let container = CKContainer(identifier: "iCloud.build.bru.Gladys")
