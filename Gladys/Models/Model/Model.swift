56380f13b9b246c22b97878cea6d852199e97638
Paul Tsochantaris <ptsochantaris@icloud.com>
2018-07-19 08:50:02 +0100

    Improved messaging in case of file coordination errors

--- Gladys/Models/Model/Model.swift
+++ Gladys/Models/Model/Model.swift
@@ -36,61 +36,82 @@ final class Model {
 		// withoutChanges because we only signal the provider after we have saved
 		coordinator.coordinate(readingItemAt: itemsDirectoryUrl, options: .withoutChanges, error: &coordinationError) { url in
 
-			if FileManager.default.fileExists(atPath: url.path) {
-				do {
-
-					var shouldLoad = true
-					if let dataModified = modificationDate(for: url) {
-						if dataModified == dataFileLastModified {
-							shouldLoad = false
-						} else {
-							dataFileLastModified = dataModified
-						}
-					}
-					if shouldLoad {
-						log("Needed to reload data, new file date: \(dataFileLastModified)")
-						didLoad = true
-
-						let start = Date()
-
-						let d = try Data(contentsOf: url.appendingPathComponent("uuids"))
-						let itemCount = d.count / 16
-						var newDrops = [ArchivedDropItem]()
-						newDrops.reserveCapacity(itemCount)
-						var c = 0
-						let decoder = JSONDecoder()
-						while c < d.count {
-							let d0 = d[c]; let d1 = d[c+1]; let d2 = d[c+2]; let d3 = d[c+3]
-							let d4 = d[c+4]; let d5 = d[c+5]; let d6 = d[c+6]; let d7 = d[c+7]
-							let d8 = d[c+8]; let d9 = d[c+9]; let d10 = d[c+10]; let d11 = d[c+11]
-							let d12 = d[c+12]; let d13 = d[c+13]; let d14 = d[c+14]; let d15 = d[c+15]
-							let u = UUID(uuid: (d0, d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11, d12, d13, d14, d15))
-							c += 16
-							let dataPath = url.appendingPathComponent(u.uuidString)
-							if let data = try? Data(contentsOf: dataPath), let item = try? decoder.decode(ArchivedDropItem.self, from: data) {
-								newDrops.append(item)
-							}
-						}
-						drops = newDrops
-						log("Load time: \(-start.timeIntervalSinceNow) seconds")
-					} else {
-						log("No need to reload data")
-					}
-				} catch {
-					log("Loading Error: \(error)")
-					loadingError = error as NSError
-				}
-			} else {
+			if !FileManager.default.fileExists(atPath: url.path) {
 				drops = []
 				log("Starting fresh store")
+				return
+			}
+
+			do {
+				var shouldLoad = true
+				if let dataModified = modificationDate(for: url) {
+					if dataModified == dataFileLastModified {
+						shouldLoad = false
+					} else {
+						dataFileLastModified = dataModified
+					}
+				}
+				if shouldLoad {
+					log("Needed to reload data, new file date: \(dataFileLastModified)")
+					didLoad = true
+
+					let start = Date()
+
+					let d = try Data(contentsOf: url.appendingPathComponent("uuids"))
+					let itemCount = d.count / 16
+					var newDrops = [ArchivedDropItem]()
+					newDrops.reserveCapacity(itemCount)
+					var c = 0
+					let decoder = JSONDecoder()
+					while c < d.count {
+						let d0 = d[c]; let d1 = d[c+1]; let d2 = d[c+2]; let d3 = d[c+3]
+						let d4 = d[c+4]; let d5 = d[c+5]; let d6 = d[c+6]; let d7 = d[c+7]
+						let d8 = d[c+8]; let d9 = d[c+9]; let d10 = d[c+10]; let d11 = d[c+11]
+						let d12 = d[c+12]; let d13 = d[c+13]; let d14 = d[c+14]; let d15 = d[c+15]
+						let u = UUID(uuid: (d0, d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11, d12, d13, d14, d15))
+						c += 16
+						let dataPath = url.appendingPathComponent(u.uuidString)
+						if let data = try? Data(contentsOf: dataPath), let item = try? decoder.decode(ArchivedDropItem.self, from: data) {
+							newDrops.append(item)
+						}
+					}
+					drops = newDrops
+					log("Load time: \(-start.timeIntervalSinceNow) seconds")
+				} else {
+					log("No need to reload data")
+				}
+			} catch {
+				log("Loading Error: \(error)")
+				loadingError = error as NSError
 			}
 		}
 
-		if let e = coordinationError ?? loadingError {
-			log("Error in loading coordination: \(e.finalDescription)")
+		if var e = loadingError {
+			log("Error in loading: \(e)")
 			#if MAINAPP || MAC
+			if let underlyingError = e.userInfo[NSUnderlyingErrorKey] as? NSError {
+				e = underlyingError
+			}
 			DispatchQueue.main.async {
-				genericAlert(title: "Loading Error (Code: \(e.code))", message: "This app's data store seems corrupt. The message received from the OS is:\n\n\"\(e.localizedDescription)\".\n\nPlease report this to the developer.") {
+				genericAlert(title: "Loading Error (Code: \(e.code))",
+				message: "This app's data store is not accessible. The message from iOS is:\n\n\(e.localizedDescription) - \(itemsDirectoryUrl.path)\n\nIf you keep getting this error, please try restarting your device, as some data may be locked by iOS.\n\nIf this error persists, please report it to the developer.") {
+					abort()
+				}
+			}
+			return
+			#else
+			abort()
+			#endif
+			
+		} else if var e = coordinationError {
+			log("Error in file coordinator: \(e)")
+			#if MAINAPP || MAC
+			if let underlyingError = e.userInfo[NSUnderlyingErrorKey] as? NSError {
+				e = underlyingError
+			}
+			DispatchQueue.main.async {
+				genericAlert(title: "Loading Error (Code: \(e.code))",
+				message: "Could not communicate with an extension. The message from iOS is:\n\n\(e.localizedDescription)\n\nIf you keep getting this error, please try restarting your device, as iOS may not have updated some Gladys extensions yet.\n\nIf this error persists, please report it to the developer.") {
 					abort()
 				}
 			}
