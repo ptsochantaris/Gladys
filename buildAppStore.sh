05f2559be2f4a729459297e9043dc66cdd87006e
Paul Tsochantaris <ptsochantaris@icloud.com>
2018-10-14 19:07:02 +0100

    App Store build and submit script

---
+++ buildAppStore.sh
@@ -0,0 +1,9 @@
+BUILD=`/usr/libexec/PlistBuddy -c "Print :CFBundleVersion" "Gladys/Info.plist"`
+BUILD=$((BUILD + 1))
+
+echo "Will send up build $BUILD..."
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $BUILD" "Gladys/Info.plist"
+
+xcodebuild clean -workspace Gladys.xcworkspace -scheme "Gladys" | xcpretty
+xcodebuild archive -workspace Gladys.xcworkspace -scheme "Gladys" -destination generic/platform=iOS -archivePath ~/Desktop/gladys-release.xcarchive | xcpretty
+xcodebuild -exportArchive -archivePath ~/Desktop/gladys-release.xcarchive -exportPath ~/Desktop/GladysAppStore -exportOptionsPlist exportAppStoreOptions.plist
