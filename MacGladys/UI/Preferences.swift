07ada53b94f195be28b29a365e7e40a7aec811f3
Paul Tsochantaris <ptsochantaris@icloud.com>
2020-04-26 22:02:05 +0100

    macOS: Prefs layout cleanup

--- MacGladys/UI/Preferences.swift
+++ MacGladys/UI/Preferences.swift
@@ -50,7 +50,8 @@ final class Preferences: NSViewController {
         kVK_ANSI_A, kVK_ANSI_B, kVK_ANSI_C, kVK_ANSI_D, kVK_ANSI_E, kVK_ANSI_F, kVK_ANSI_G, kVK_ANSI_H,
         kVK_ANSI_I, kVK_ANSI_J, kVK_ANSI_K, kVK_ANSI_L, kVK_ANSI_M, kVK_ANSI_N, kVK_ANSI_O, kVK_ANSI_P,
         kVK_ANSI_Q, kVK_ANSI_R, kVK_ANSI_S, kVK_ANSI_T, kVK_ANSI_U, kVK_ANSI_V, kVK_ANSI_W, kVK_ANSI_X,
-        kVK_ANSI_Y, kVK_ANSI_Z
+        kVK_ANSI_Y, kVK_ANSI_Z, kVK_ANSI_0, kVK_ANSI_1, kVK_ANSI_2, kVK_ANSI_3, kVK_ANSI_4, kVK_ANSI_5,
+        kVK_ANSI_6, kVK_ANSI_7, kVK_ANSI_8, kVK_ANSI_9
     ]
     
     private func createStringForKey(keyCode: CGKeyCode) -> String? {
@@ -116,7 +117,7 @@ final class Preferences: NSViewController {
 			m.removeAllItems()
 			m.addItem(withTitle: "None", action: #selector(hotkeyCharChanged), keyEquivalent: "")
             var count = 0
-            for char in "ABCDEFGHIJKLMNOPQRSTUVWXYZ" {
+            for char in "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789" {
                 let code = keyMap[count]
                 if let char = createStringForKey(keyCode: CGKeyCode(code)) {
                     m.addItem(withTitle: char, action: #selector(hotkeyCharChanged), keyEquivalent: "")
