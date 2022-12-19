6a2bfc2fde9e1b132646fbb0654426eb57915231
Paul Tsochantaris <ptsochantaris@icloud.com>
2022-12-19 12:42:34 +0000

    watchOS cleanup

---
+++ GladysWatch/Drop.swift
@@ -0,0 +1,156 @@
+import SwiftUI
+import WatchConnectivity
+
+final class ImageCache {
+    private static let cacheDir = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+
+    private static let accessKeys = Set([URLResourceKey.contentAccessDateKey])
+
+    static func setImageData(_ data: Data, for key: String) {
+        let imageUrl = cacheDir.appendingPathComponent(key)
+        do {
+            try data.write(to: imageUrl)
+        } catch {
+            print("Error writing data to: \(error.localizedDescription)")
+        }
+    }
+
+    static func imageData(for key: String) -> Data? {
+        var imageUrl = cacheDir.appendingPathComponent(key)
+        if FileManager.default.fileExists(atPath: imageUrl.path) {
+            var v = URLResourceValues()
+            let now = Date()
+            v.contentModificationDate = now
+            v.contentAccessDate = now
+            try? imageUrl.setResourceValues(v)
+            return try? Data(contentsOf: imageUrl, options: .mappedIfSafe)
+        }
+        return nil
+    }
+
+    static func trimUnaccessedEntries() {
+        if let cachedFiles = try? FileManager.default.contentsOfDirectory(at: cacheDir, includingPropertiesForKeys: nil, options: [.skipsHiddenFiles, .skipsPackageDescendants, .skipsSubdirectoryDescendants]) {
+            let now = Date()
+            let fm = FileManager.default
+            for file in cachedFiles {
+                if let accessDate = (try? file.resourceValues(forKeys: accessKeys))?.contentAccessDate {
+                    if now.timeIntervalSince(accessDate) > (3600 * 24 * 7) {
+                        try? fm.removeItem(at: file)
+                    }
+                }
+            }
+        }
+    }
+}
+
+final class Drop: Identifiable, ObservableObject {
+    let id: String
+    let title: String
+    let imageDate: Date
+
+    enum ImageState {
+        case none, loading, empty, loaded(image: UIImage)
+    }
+    
+    indirect enum UIState {
+        case noText, text, menu(over: UIState), action(label: String)
+    }
+
+    @Published var imageState = ImageState.none
+    @Published var uiState = UIState.text
+
+    init?(json: [String: Any]) {
+        guard let id = json["u"] as? String,
+              let title = json["t"] as? String,
+              let imageDate = json["d"] as? Date
+        else {
+            return nil
+        }
+        self.id = id
+        self.title = title
+        self.imageDate = imageDate
+    }
+    
+    func fetchImage() {
+        guard case .none = imageState else {
+            return
+        }
+        
+        let cacheKey = id + String(imageDate.timeIntervalSinceReferenceDate) + ".dat"
+        if let data = ImageCache.imageData(for: cacheKey), let i = UIImage(data: data) {
+            imageState = .loaded(image: i)
+            return
+        }
+
+        imageState = .loading
+
+        let screen = WKInterfaceDevice.current()
+        let size = CGSize(width: screen.screenBounds.width, height: screen.screenBounds.height)
+        WCSession.default.sendMessage(["image": id, "width": size.width, "height": size.height], replyHandler: { reply in
+            if let r = reply["image"] as? Data {
+                if let i = UIImage(data: r) {
+                    ImageCache.setImageData(r, for: cacheKey)
+                    Task { @MainActor in
+                        self.imageState = .loaded(image: i)
+                    }
+                }
+            }
+        }, errorHandler: { _ in
+            Task { @MainActor in
+                self.imageState = .empty
+            }
+        })
+    }
+    
+    func viewOnDeviceSelected() {
+        uiState = .action(label: "Opening item on the phone app")
+        WCSession.default.sendMessage(["view": id]) { _ in
+            Task { @MainActor in
+                self.uiState = .text
+            }
+        } errorHandler: { _ in
+            Task { @MainActor in
+                self.uiState = .text
+            }
+        }
+    }
+
+    func copySelected() {
+        uiState = .action(label: "Copying")
+        WCSession.default.sendMessage(["copy": id]) { _ in
+            Task { @MainActor in
+                self.uiState = .text
+            }
+        } errorHandler: { _ in
+            Task { @MainActor in
+                self.uiState = .text
+            }
+        }
+    }
+
+    func moveToTopSelected() {
+        uiState = .action(label: "Moving to the top of the list")
+        WCSession.default.sendMessage(["moveToTop": id]) { _ in
+            Task { @MainActor in
+                self.uiState = .text
+            }
+        } errorHandler: { _ in
+            Task { @MainActor in
+                self.uiState = .text
+            }
+        }
+    }
+
+    func deleteSelected() {
+        uiState = .action(label: "Deleting")
+        WCSession.default.sendMessage(["delete": id]) { _ in
+            Task { @MainActor in
+                self.uiState = .text
+            }
+        } errorHandler: { _ in
+            Task { @MainActor in
+                self.uiState = .text
+            }
+        }
+    }
+}
---
+++ GladysWatch/GladysWatchModel.swift
@@ -0,0 +1,67 @@
+import WatchConnectivity
+import GladysFramework
+
+final class GladysWatchModel: NSObject, ObservableObject, WCSessionDelegate {
+    @Published var reportedCount = 0
+    @Published var dropList = [Drop]()
+
+    static let shared = GladysWatchModel()
+
+    enum State {
+        case loading, empty, list
+    }
+    
+    var state = State.loading
+
+    private func extractDropList(from context: [String: Any]) -> ([[String: Any]], Int) {
+        if
+            let reportedCount = context["total"] as? Int,
+            let compressedData = context["dropList"] as? Data,
+            let uncompressedData = compressedData.data(operation: .decompress),
+            let itemInfo = SafeArchiving.unarchive(uncompressedData) as? [[String: Any]] {
+            var count = 1
+            let list = itemInfo.map { dict -> [String: Any] in
+                var d = dict
+                d["it"] = "\(count) of \(reportedCount)"
+                count += 1
+                return d
+            }
+            return (list, reportedCount)
+        } else {
+            return ([], 0)
+        }
+    }
+
+    private func receivedInfo(_ info: [String: Any]) {
+        let (dropList, reportedCount) = extractDropList(from: info)
+        Task { @MainActor in
+            self.reportedCount = reportedCount
+            self.dropList = dropList.compactMap { Drop(json: $0) }
+            ComplicationDataSource.reloadComplications()
+            ImageCache.trimUnaccessedEntries()
+            if dropList.isEmpty {
+                state = .empty
+            } else {
+                state = .list
+            }
+        }
+    }
+
+    func session(_: WCSession, didReceiveUserInfo userInfo: [String: Any] = [:]) {
+        receivedInfo(userInfo)
+    }
+
+    func getFullUpdate(session: WCSession) {
+        if session.activationState == .activated {
+            session.sendMessage(["update": "full"]) { [weak self] info in
+                self?.receivedInfo(info)
+            }
+        }
+    }
+
+    func session(_ session: WCSession, activationDidCompleteWith _: WCSessionActivationState, error _: Error?) {
+        getFullUpdate(session: session)
+    }
+
+    func session(_: WCSession, didReceiveApplicationContext _: [String: Any]) {}
+}
