46c6a47bef5c83ace337dfb89f7c0a7862f86799
Paul Tsochantaris <ptsochantaris@icloud.com>
2023-01-19 19:09:58 +0000

    Common framework

--- GladysCommon/Package.swift
+++ GladysCommon/Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version: 5.7
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -11,9 +10,9 @@ let package = Package(
         .watchOS(.v8)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "GladysCommon",
+            type: .dynamic,
             targets: ["GladysCommon"]
         )
     ],
@@ -23,8 +22,6 @@ let package = Package(
         .package(url: "https://github.com/cezheng/Fuzi", from: "3.0.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "GladysCommon",
             dependencies: ["ExceptionCatcher",
