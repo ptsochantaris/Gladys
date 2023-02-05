6264ba37d55f34df3d8e21cdcd9ffe966339bed7
Paul Tsochantaris <ptsochantaris@icloud.com>
2023-02-05 18:05:31 +0000

    Consolidation

--- GladysCommon/Sources/GladysCommon/ArchivedItem.swift
+++ GladysCommon/Sources/GladysCommon/ArchivedItem.swift
@@ -949,4 +949,17 @@ extension ArchivedItem: Hashable, DisplayImageProviding {
         record["highlightColor"] = highlightColor.rawValue
         return record
     }
+    
+    public var backgroundInfoObject: Any? {
+        var currentItem: Any?
+        var currentPriority = -1
+        for item in components {
+            let (newItem, newPriority) = item.backgroundInfoObject
+            if let newItem, newPriority > currentPriority {
+                currentItem = newItem
+                currentPriority = newPriority
+            }
+        }
+        return currentItem
+    }
 }
